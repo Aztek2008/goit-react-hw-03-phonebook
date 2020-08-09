d050ea96752f35f6e0d204e6bf2bc96651650bc8
Aztek2008 <Aztek2008@gmail.com>
2020-08-09 17:30:00 +0300

    Updates

---
+++ precache-manifest.42cc0a82c91c3c4acb725640ce2e1bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7276251e903b6182ca2e5553f5aeaa",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "7c98ac48d75aa075cba1",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.b198b6ac.chunk.css"
+  },
+  {
+    "revision": "f46f76a94562f8aff6f1",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f19728d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f19728d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c98ac48d75aa075cba1",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.6e552b2c.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
